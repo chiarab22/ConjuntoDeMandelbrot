386151b79bace5b5e6cd39fd5ae464ceb23dcc8d
Chiara Boccaletti <cbocc@myuax.com>
2022-10-19 09:28:14 +0200

    Subido Conjunto de mandelbrot actualizado

--- ConjuntodeMandelbrot/src/Main.java
+++ ConjuntodeMandelbrot/src/Main.java
@@ -1,9 +1,12 @@
-import conjuntodemandelbrot.Mandelbrot;
+import conjuntodemandelbrot.*;
 
 public class Main {
     public static void main(String[] args){
     Mandelbrot mandelbrot = new Mandelbrot();
     mandelbrot.dibujarMandelbrot(args);
+    Mandelbrot1 mandelbrot1 = new Mandelbrot1();
+    mandelbrot1.dibujaMandelbrot(args);
+
 
 
     }
